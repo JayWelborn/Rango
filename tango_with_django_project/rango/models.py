9253ea443ed2effbb9b8a30be399b793e5316ca1
Jay Welborn <jesse.welborn@gmail.com>
2017-05-31 10:12:55 +0200

    made two templates and routed two test urls

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    """
+    Category is a class for displaying websites with similar content.
+    name = name of the category
+    slug = name slugified for URL purposes
+    pub_date = date the category was published
+    pages = reverse lookup for pages related to a category
+    """
+
+    name = models.CharField(max_length=128, unique=True)
+    slug = models.SlugField(max_length=128, unique=True)
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    """
+    Page will store info for displaying pages within a category
+    category = reference to category to which the page belongs
+    title = page title
+    url = page url
+    views = number of times users have viewed the page
+    """
+    
+    category = models.ForeignKey(Category, related_name='pages')
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+        
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -14,7 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_DIR = os.path.join(PROJECT_DIR, 'templates')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -58,12 +61,13 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -122,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files (user-updloaded photos, audio, etc.)
+
+MEDIA_URL = '/media/'
