c5aaf694246b73a461c0523db3a0972ffc544bb5
Jay Welborn <jesse.welborn@gmail.com>
2017-06-02 21:02:43 +0200

    changed templates to inherit from base.html

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'rango'
+
 urlpatterns = [
-    url(r'^about', views.about, name='about'),
+    url(r'^about/$', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
